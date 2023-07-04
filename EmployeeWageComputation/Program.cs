6b34b916ce33739065d0f62c5a217b03a5df3e8b
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-04 20:19:48 +0530

    [mohitkumar9884] modifying EmployeeAttendance Program

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿namespace EmployeeWageComputation
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Welcome To EmployeeWageComputation");
+        private static void Main(string[] args) 
+        {
+            const int Is_Present = 0;
+            
+            Console.WriteLine("Welcome To EmployeeWageComputation");
+            Random random = new Random();
+            int employeeInput=random.Next(0,2);// 0 or 1
+            Console.WriteLine("Random value:{0}", employeeInput);
+
+            if (employeeInput == Is_Present)
+            {
+                Console.WriteLine("Employee Is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Not Present");
+            }
+        }
     }
 }

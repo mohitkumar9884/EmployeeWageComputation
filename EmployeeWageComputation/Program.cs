10144842bafbd1fb51ccd080045f23baaec569b0
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-05 14:26:19 +0530

    [mohitkumar9884] adding/creating Employee Daily Wage Calculation

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,7 +1,31 @@
-﻿internal class Program
+﻿namespace EmployeeWageComputation
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Welcome To EmployeeWageComputation");
+        private static void Main(string[] args)
+        {
+            const int Is_Present = 0;
+            const int RATE_PER_HOUR = 20;
+            int empHr = 0;
+            int empWage = 0;
+
+            Console.WriteLine("Welcome To EmployeeWageComputation");
+            Random random = new Random();
+            int employeeInput = random.Next(0, 2);// 0 or 1
+            Console.WriteLine("Random value:{0}", employeeInput);
+
+            if (employeeInput == Is_Present)
+            {
+                Console.WriteLine("Employee Is Present");
+                empHr = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Not Present");
+            }
+            empWage = empHr*RATE_PER_HOUR;
+            Console.WriteLine("Dialy Employee WAGE Is:{0}", empWage);
+
+        }
     }
 }

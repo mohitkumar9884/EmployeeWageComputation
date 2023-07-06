d08b108f0f48ed2ef0bbda85dfb96e2f2a5a261a
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-06 13:33:19 +0530

    [mohitkumar9884] adding/creating PartTime EmployeeWageComputation

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -1,7 +1,38 @@
-﻿internal class Program
+﻿namespace EmployeeWageComputation
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Welcome To EmployeeWageComputation");
+        private static void Main(string[] args)
+        {
+            const int FULL_TIME = 0;
+            const int PART_TIME = 2;
+            const int RATE_PER_HOUR = 20;
+            int empHr = 0;
+            int empWage = 0;
+
+            Console.WriteLine("Welcome To EmployeeWageComputation");
+            Random random = new Random();
+            int employeeInput = random.Next(0, 3);// 0 or 1 or 2
+
+            Console.WriteLine("Random value:{0}", employeeInput);
+
+            if (employeeInput == FULL_TIME)
+            {
+                Console.WriteLine("FullTime Employee Is Present");
+                empHr = 8;
+            }
+            else if (employeeInput== PART_TIME)
+            {
+                Console.WriteLine("PartTime Employee Is Present");
+                empHr = 4;
+            }
+            else
+            {
+                Console.WriteLine("Employee Is Not Present");
+            }
+            empWage = empHr * RATE_PER_HOUR;
+            Console.WriteLine("Dialy Employee WAGE Is:{0}", empWage);
+        }
     }
+    
 }

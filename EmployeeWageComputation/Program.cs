b6243870f56abb034b2a2e29759b4ad76ecb6004
mohitkumar9884 <mohitkumar9884@gmail.com>
2023-07-06 16:54:33 +0530

    [mohitkumar9884] adding/modifying MonthlyWageCalculation

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -7,33 +7,40 @@
             const int FULL_TIME = 0;
             const int PART_TIME = 2;
             const int RATE_PER_HOUR = 20;
+            const int WORKING_DAYS = 20;
             int empHr = 0;
             int empWage = 0;
+            int totalWage= 0;
+            int day = 1;
 
             Console.WriteLine("Welcome To EmployeeWageComputation");
             Random random = new Random();
-            int employeeInput = random.Next(0, 3);// 0 or 1 or 2
-
-            Console.WriteLine("Random value:{0}", employeeInput);
-            switch (employeeInput)
+            for (day = 1; day <= WORKING_DAYS; day++)
             {
-                case FULL_TIME:
-                    //code block
-                    Console.WriteLine("FullTime Employee Is Present");
-                    empHr = 8;
-                    break;
-                case PART_TIME:
-                    //code block
-                    Console.WriteLine("PartTime Employee Is Present");
-                    empHr = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee Is Not Present");
-                    break;          
+                int employeeInput = random.Next(0, 3);// 0 or 1 or 2
+                //Console.WriteLine("Random value:{0}", employeeInput);
+                switch (employeeInput)
+                {
+                    case FULL_TIME:
+                        //code block
+                        //Console.WriteLine("FullTime Employee Is Present");
+                        empHr = 8;
+                        break;
+                    case PART_TIME:
+                        //code block
+                        //Console.WriteLine("PartTime Employee Is Present");
+                        empHr = 4;
+                        break;
+                    default:
+                        //Console.WriteLine("Employee Is Not Present");
+                        empHr = 0;
+                        break;
+                }
+                empWage = empHr * RATE_PER_HOUR;
+                totalWage = totalWage + empWage;
+                Console.WriteLine("Day{0} Employee WAGE Is:{1}", day, empWage);
             }
-            empWage = empHr*RATE_PER_HOUR;
-            empWage = empHr * RATE_PER_HOUR;
-            Console.WriteLine("Dialy Employee WAGE Is:{0}", empWage);
+            Console.WriteLine("Total Wage for {0} days is :{1}", day - 1, totalWage);
         }
     }
     
